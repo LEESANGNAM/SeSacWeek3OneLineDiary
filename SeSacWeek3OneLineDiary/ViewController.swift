10b96b9b9e6de8e4679561b62e48d7cc9e3dd6d9
LEESANGNAM <fgh0116@naver.com>
2023-07-31 09:40:06 +0900

    Initial Commit

---
+++ SeSacWeek3OneLineDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSacWeek3OneLineDiary
+//
+//  Created by 이상남 on 2023/07/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
