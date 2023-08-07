ea9a33b0aac23f4a52377f55dce934c00b5ee01c
LEESANGNAM <fgh0116@naver.com>
2023-08-07 12:02:43 +0900

    API, Alamofire

--- SeSacWeek3OneLineDiary/LottoViewController.swift
+++ SeSacWeek3OneLineDiary/LottoViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class LottoViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDataSource {
   
     @IBOutlet weak var numberTextField: UITextField!
     
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var bonuNumberLabel: UILabel!
     
     let pickerView = UIPickerView()
     
+    var number: Int = 0
     
     var list: [Int] = Array(1...1100).reversed()
     //Array(repeating: 100, count: 1000)
@@ -23,17 +29,49 @@ class LottoViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDat
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        print("1")
+        print("2")
+        callRequest(number: 1079)
+        
+        print("4")
+        
         pickerView.delegate = self
         pickerView.dataSource = self
         
         numberTextField.inputView = pickerView
         numberTextField.tintColor = .clear
-        // Do any additional setup after loading the view.
+        
+        print("5")
     }
+    
+    func callRequest(number: Int){
+        let url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=\(number)"
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+                print("3")
+                
+                let date = json["drwNoDate"].stringValue
+                let bounsNumber = json["bnusNo"].intValue
+                
+                print("date",date,"bounsNumber",bounsNumber)
+                
+                self.dateLabel.text = date
+                self.bonuNumberLabel.text = "\(bounsNumber)번"
+                
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+   
+    
     
     //돌아가는거 몇개
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 2
+        return 1
     }
     // 셀 몇개
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
@@ -43,6 +81,9 @@ class LottoViewController: UIViewController,UIPickerViewDelegate,UIPickerViewDat
     // 선택
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         numberTextField.text = "\(list[row])"
+        number = list[row]
+        
+        callRequest(number: number)
     }
     //
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
